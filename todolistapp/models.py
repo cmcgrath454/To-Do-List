451371518f8b3cd3afd90edb52fdc95e66625d3d
Colin McGrath <cmcgrath454@gmail.com>
2022-03-24 08:52:52 -0500

    Added Database Models

--- todolistapp/models.py
+++ todolistapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class List(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Task(models.Model):
+    list = models.ForeignKey(List, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    desc = models.TextField(null=True, blank=True)
+    date_scheduled = models.DateField(auto_now_add=True)
+    date_due = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
